327a9793b959cf3713386db9a75c98e3fae45c3e
Fahadk999 <fahadkxyz18@gmail.com>
2026-08-27 23:23:31 +0500

    added gameover screen image

--- src/gamestates/overstate.py
+++ src/gamestates/overstate.py
@@ -1,11 +1,15 @@
 from src.ui.text import Text
+from src.ui.imageloader import LoadImage
 
 class OverState:
     def __init__(self, screenWidth, screenHeight) -> None:
         # over title , retry option, and score display
-        self.overTxt = Text("GAE", screenWidth//2, screenHeight//2)
+        self.screenWidth = screenWidth
+        self.screenHeight = screenHeight
+        self.overDisplay = LoadImage("assets/gameover.png", 1, self.screenWidth//2, self.screenHeight//2)
+
 
     def draw (self, screen):
-        self.overTxt.draw(screen)
+        self.overDisplay.draw(screen)
 
     
